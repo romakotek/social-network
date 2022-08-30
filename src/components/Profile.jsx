1e4687951ec0b3b4504940831615d804f27916de
romakotek <buraby1@gmail.com>
2022-08-30 23:53:03 +0600

    first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg' />
+            </div>
+            <div>
+                ava+description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    Post#1
+                </div>
+                <div>
+                    Post#2
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Profile;
