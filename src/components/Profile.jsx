c7b33d14c5602121119df54019003dce2c07e5ae
romakotek <buraby1@gmail.com>
2022-08-31 15:28:03 +0600

    added css modules

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={s.content}>
             <div>
                 <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg' />
             </div>
@@ -14,15 +15,15 @@ const Profile = () => {
                 <div>
                     New post
                 </div>
-                <div>
+                <div className={s.item}>
                     Post#1
                 </div>
-                <div>
+                <div className={s.item}>
                     Post#2
                 </div>
             </div>
-
         </div>
+
     )
 }
 
