7ad88880431d5c23a3d63ffbf9d4f9764fbd806a
romakotek <buraby1@gmail.com>
2022-09-02 16:26:52 +0600

    added routes to all pages on navbar

--- src/App.js
+++ src/App.js
@@ -2,21 +2,32 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header'
 import Navbar from './components/Navbar/Navbar';
-import Profile from './components/Profile/Profile';
-import Dialogs from './components/Dialogs/Dialogs';
+import Profile from './components/Pages/Profile/Profile';
+import Dialogs from './components/Pages/Dialogs/Dialogs';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import News from './components/Pages/News/News';
+import Music from './components/Pages/Music/Music';
+import Settings from './components/Pages/Settings/Settings';
+
 
 function App() {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <div className="app-wrapper-content">
-        <Route />
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+        <div className="app-wrapper-content">
+          <Routes>
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/dialogs' element={<Dialogs />} />
+            <Route path='/news' element={<News />} />
+            <Route path='/music' element={<Music />} />
+            <Route path='/settings' element={<Settings />} />
 
-        <Dialogs />
-        {/* <Profile /> */}
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
